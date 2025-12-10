6ea6ffe653ec729c73f9396821804facf9939f3b
joeden <joedenlem@gmail.com>
2025-12-10 19:26:00 +0800

    Added custom identity logic

--- FurryFriendsAdoption/Program.cs
+++ FurryFriendsAdoption/Program.cs
@@ -34,6 +34,7 @@ builder.Services.AddAuthentication(options =>
     .AddIdentityCookies();
 
 builder.Services.AddIdentityCore<FurryFriendsAdoptionUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<FurryFriendsAdoptionContext>()
     .AddSignInManager()
     .AddDefaultTokenProviders();
